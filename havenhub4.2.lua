17f3c1cd17c0089be6536f641057cafc1d420283
moonthecoder <94987124+moonthecoder@users.noreply.github.com>
2021-12-29 18:49:13 +0100

    havenhubv4.2

--- havenhub4.2.lua
+++ havenhub4.2.lua
@@ -1,6 +1,16 @@
 -- https://discord.io/bittersweet
+game:GetService("StarterGui"):SetCore("SendNotification",{     
+	Title = "CREDITS",     
+	Text = "The new script for v2.6 is in our discord, a invite url has been copied or just go to discord.io/bittersweet.";
+	Button1 = "Ok!",
+	Duration = 300000000
+  })
 
-game:GetService("StarterGui"):SetCore("SendNotification",{Title="Outdated Version",Text="The new script for v2.6 is in our discord, a invite url has been copied or just go to discord.io/bittersweet",Button1="Ok!",Duration=30000000})
+game:GetService("StarterGui"):SetCore("SendNotification",{
+    Title = "Этот хаб старый"
+    Text = "Этот хаб старый, присоединяйтесь к нашему разногласию, чтобы получить новый, он скопирован."
+    Button1 = "Ok!",
+    Duration = 300000000
+    })
+
 setclipboard("https://discord.gg/bEB3GAqThV")
--- Russian version. 
-game:GetService("StarterGui"):SetCore("SendNotification",{Title="Outdated Version",Text="Этот хаб старый, присоединяйтесь к нашему разногласию, чтобы получить новый, он скопирован.",Button1="👍",Duration=30000000})
